d051405b23b240692f01af0621120a2067797be9
tech-student01 <2022php42-student002@la-study.com>
2022-05-19 11:10:31 +0900

    headerを追加

---
+++ resources/views/commons/nav.blade.php
@@ -0,0 +1,25 @@
+{{-- ヘッダーのコピペです。 --}}
+
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+{{-- ロゴの横のメニューです --}}
+      {{-- <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('products.index') }}">商品一覧</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('products.index') }}">商品一覧</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('products.index') }}">商品一覧</a>
+          </li>
+        </ul>
+      </div> --}}
+    </div>
+  </nav>
--- resources/views/コピペ用.blade.php
+++ resources/views/コピペ用.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>宿一覧</h1>
+    
 @endsection
