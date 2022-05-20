9778bb221a3b98115f4e03a8fd6f982aec353cf1
morisuzuka-2022 <2022php42-student007@la-study.com>
2022-05-20 11:42:46 +0900

    ？

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [UserController::class,'index']);
+
