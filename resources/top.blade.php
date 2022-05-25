64377ca90bc101b5ae63207105c4d4f17a95090d
morisuzuka-2022 <2022php42-student007@la-study.com>
2022-05-25 16:13:00 +0900

    !

--- resources/top.blade.php
+++ resources/top.blade.php
@@ -87,3 +87,11 @@
     </div>
     </div>
 	@endsection
+
+<?php /*
+@extends('layouts.app')
+
+@section('content')
+    <p><a href="{{ route('hotels.index') }}">宿情報検索へ</a></p>
+    <p><a href="">会員情報検索へ</a></p>
+@endsection
