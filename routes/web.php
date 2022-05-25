8ab1e8608b82662aac1e2abbfbac8b7786d3b592
php2022takasaki <2022php42-student019@la-study.com>
2022-05-25 14:56:24 +0900

    変更

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,8 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\PlanController;
-<<<<<<< HEAD
-use App\Http\Controllers\MemberInfoController;
-=======
 use App\Http\Controllers\HomeController;
 
->>>>>>> main
 use Illuminate\Http\Request;
 
 
@@ -36,29 +32,20 @@ Route::post('/conf', [HomeController::class,'confirm'])->name('conf');
 Route::post('/complete', [HomeController::class,'complete'])->name('complete');
 
 Route::get('/home', [HotelController::class,'index'])->name('home');
-<<<<<<< HEAD
-=======
 
-//宿予約
-Route::post('/create', [OrderController::class, 'create'])->name('orders.form.create');
-Route::post('/orders/confirm', [OrderController::class, 'confirm'])->name("orders.confirm");
-Route::post('/orders/complete', [OrderController::class, 'complete'])->name("orders.complete");
-
->>>>>>> main
 //Route::post('/conf', function(Request $request){
 //});
+
 // 最終的に消すやつ（こばやし）
 Route::get('/admin', function () {
     return view('admin/top');
 });
 
-Route::get('/users', [MemberInfoController::class,'index'])->name('users');
-
 //一覧と詳細表示だけ
 Route::resource('hotels', HotelController::class);
 Route::resource('plans', PlanController::class);
 Route::resource('orders', OrderController::class);
-Route::resource('users', MemberInfoController::class);
+
 // Route::group(['middleware' => ['auth']] function () {
     // ログインしてないと見れないところ、ログイン機能完成したら入れます
 // });
