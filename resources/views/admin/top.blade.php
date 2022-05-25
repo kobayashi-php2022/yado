ee702e928e5858d403dc095cf1b114c636c82640
php2022takasaki <2022php42-student019@la-study.com>
2022-05-25 14:13:17 +0900

    一部分のみとりあえず

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <p><a href="{{ route('hotels.index') }}">宿情報検索へ</a></p>
-    <p><a href="">会員情報検索へ</a></p>
+    <p><a href="{{ route('users.index') }}">会員情報検索へ</a></p>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\PlanController;
-
+use App\Http\Controllers\MemberInfoController;
 use Illuminate\Http\Request;
 
 
@@ -28,20 +28,20 @@ Route::post('/conf', [UserController::class,'confirm'])->name('conf');
 Route::post('/complete', [UserController::class,'complete'])->name('complete');
 
 Route::get('/home', [HotelController::class,'index'])->name('home');
-
 //Route::post('/conf', function(Request $request){
 //});
-
 // 最終的に消すやつ（こばやし）
 Route::get('/admin', function () {
     return view('admin/top');
 });
 
+Route::get('/users', [MemberInfoController::class,'index'])->name('users');
+
 //一覧と詳細表示だけ
 Route::resource('hotels', HotelController::class);
 Route::resource('plans', PlanController::class);
 Route::resource('orders', OrderController::class);
-
+Route::resource('users', MemberInfoController::class);
 // Route::group(['middleware' => ['auth']] function () {
     // ログインしてないと見れないところ、ログイン機能完成したら入れます
 // });
