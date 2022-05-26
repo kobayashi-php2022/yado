bead9efbdc80cf12967e7ea4ecbcd850e06b73c1
morisuzuka-2022 <2022php42-student007@la-study.com>
2022-05-26 17:33:07 +0900

    !

--- resources/views/reserve/index.blade.php
+++ resources/views/reserve/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app1')
 
 @section('content')
 <form action="{{ route('orders.index') }}" method="get">
--- resources/views/reserve/show.blade.php
+++ resources/views/reserve/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app1')
 
 @section('content')
 <div class="header_image">
