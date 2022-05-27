06173e4b70d1699f6954530aeccb524b632f4c05
morisuzuka-2022 <2022php42-student007@la-study.com>
2022-05-27 12:01:16 +0900

    CSSついか！

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,21 @@
-@extends('layouts.app1')
+@extends('layouts.app3')
 
-@section('title', '新宿トラベル_宿詳細画面')
+@section('title', '新宿トラベル_ログイン画面')
 @section('content')
 
-<body class="components-page">
-
-<a href="">
+<body>
+	<div class="image-container set-full-height" style="background-image: url('img/wizard-city.jpg')">
+    <a href="">
 	         <div class="logo-container">
 	            <div class="logo">
-	                <img src="img/logo.png"alt="logo">
+	                <img src="img/logo.png"alt="logo" style="width: 50%;">
 	            </div>
 	        </div>
 	    </a>
+        
+    <body class="components-page2">
 
-<nav class="navbar navbar-transparent navbar-fixed-top navbar-color-on-scroll" role="navigation">
+    <nav class="navbar navbar-transparent navbar-fixed-top navbar-color-on-scroll" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -23,118 +25,75 @@
         <span class="icon-bar bar2"></span>
         <span class="icon-bar bar3"></span>
       </button>
-     </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="navigation-doc">
-                <ul  class="nav navbar-nav navbar-right">
-                <li>
-                        <a href="#">TOPぺージへ</a>
-                    </li>
-                </ul>
-
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-        </nav>
-
-                    <div class="wrapper">
-                        <div class="header header-filter" style="background-image: url('');">
-                            <div class="container">
-                            <div class="row">
-                                    <div class="col-md-6 col-md-offset-3 text-center">
-                                @csrf
-                                <h2 style="color: white;">宿の詳細情報</h2><br>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                        </div>
-                        <div class="main main-raised">
-                            <div class="section">
-                            <div class="row">
-                                <div class="col-md-8 col-md-offset-1">
-
-                    <div class="p-3 mb-2 bg-secondary"><h5 style="font-weight: bold;">プラン詳細</h5></div>
-                            <div class="container mt-2 mb-2 border rounded">
-                            <table style="font-size: 14px;">
-                            <tr>
-                                <th>プラン名</th>
-                                <th>{{ $plan->name }}</th>
-                            </tr>
-                            <tr>
-                                <td>プランの説明</td>
-                                <td>{!! nl2br(e($plan->content)) !!}</td>
-                            </tr>
-                            <tr>
-                                <td>料金</td>
-                                <td>{{ $plan->price }}円</td>
-                            </tr>
-                            <tr>
-                                <td>部屋数</td>
-                                <td>{{ $plan->rooms_num }}部屋</td>
-                            </tr>
-                            </table>
-                            </div>
-
-                    <div class="p-3 mb-2 bg-secondary"><h5 style="font-weight: bold;">空き情報を確認する</h5></div>
-                            <div class="container mt-2 mb-2 border rounded">
-                            <p>予約したい日付と人数を入力してください</p>
-                            <form action="{{ route('plans.show', $plan->id) }}" method="get">
-                            <dl style="font-size: 14px; line-height:2;">
-                            <dt>チェックイン日</dt>
-                            <dd><input type="date" name="search_check_in" id="search_check_in" value="{{ request('search_check_in') }}"></dd>
-                            <dt>チェックアウト日</dt>
-                            <dd><input type="date" name="search_check_out" id="search_check_out" value="{{ request('search_check_out') }}"></dd>
-                            <dt>部屋数</dt>
-                            <dd><input type="number" name="search_rooms_num" id="search_rooms_num" value="{{ request('search_rooms_num') }}">部屋</dd>
-                            </dl>
-                            <input type="submit" value="検索" class="btn btn-warning">
-                        </form>
-                        <hr>
-                            </div>
-                            <div class="p-3 mb-2 bg-secondary"><h5 style="font-weight: bold;">検索結果</h5></div>
-                            <div class="container mt-2 mb-2 border rounded">
-                            @if (request('search_check_in') !== null && request('search_check_out') !== null && request('search_rooms_num') !== null) 
-                            {{-- チェックイン日のほうがチェックアウト日よりも前の日程であれば表示 --}}
-                            @if (request('search_check_in') < request('search_check_out'))
-                                {{-- まだ空き部屋があれば予約ボタン表示 --}}
-                                @if ($reserved_rooms_sum + request('search_rooms_num') <= $plan->rooms_num)
-                                    <p>この日程で予約できます。</p>
-                                    <form action="{{ route('orders.form.create') }}" method="post">
-                                        @csrf
-                                        <input type="hidden" name="plan_id" value="{{ $plan->id }}">
-                                        <input type="hidden" name="search_check_in" value="{{ request('search_check_in') }}">
-                                        <input type="hidden" name="search_check_out" value="{{ request('search_check_out') }}">
-                                        <input type="hidden" name="search_rooms_num" value="{{ request('search_rooms_num') }}">
-                                        <button type="submit">このプランを予約する</button>
-                                    </form>
-                                @else
-                                    <p>この日程では予約できません。違う日程を選択してください。</p>
-                                @endif
-                            @else
-                                <p>チェックアウト日はチェックイン日よりも後の日付を選択してください。</p>
-                            @endif
-                        @else
-                            <p>検索項目が入力されていません。</p>
-                        @endif
-                      </div>
-                    <hr/>
-                    <a onClick="history.back()">　< 戻る</a>
-                 </div>
-            </div>
-        </div>
     </div>
 
-    <style>
-        /* 追加 */
-        table {
-            margin-left: 30px;  
-            line-height: 2;
-            font-size:17px;
-        }
-        table th{
-            font-size:20px;
-        }
-	</style>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navigation-doc">
+		<ul  class="nav navbar-nav navbar-right">
+    <li>
+				<a href="#">TOPページへ</a>
+			</li>
+			<li>
+    	</ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+	    <!--   Big container   -->
+	    <div class="container">
+	        <div class="row">
+		        <div class="col-sm-8 col-sm-offset-2">
+		            <div class="wizard-container">
+		                <div class="card wizard-card" data-color="purple" id="wizard">
+                            <div class="wizard-header">
+		                        	<h3 class="wizard-title">
+		                        		ログイン
+		                        	</h3>
+		                    	</div>
+								<div class="wizard-navigation">
+									<ul>
+			                <li><a href="#details" data-toggle="tab">ログイン情報の入力</a></li>
+			                <li><a data-toggle="tab"></a></li>
+			            </ul>
+								</div><br><br>
+          <div class="cotainer">
+          <form action="{{route('login')}}" method="post" onsubmit="">
+          @csrf
+            <div class="form-group row">
+              <label for="email" class="col-md-3 col-form-label text-md-right"></label>
+              <div class="col-md-6">
+                <input type="email" id="email" class="form-control" name="email" placeholder="メールアドレス" value="{{old('email')}}" required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="password" class="col-md-3 col-form-label text-md-right"></label>
+              <div class="col-md-6">
+                <input type="password" id="password" class="form-control" name="password" placeholder="パスワード" required>
+              </div>
+            </div>
+            <br>
+            <div style="text-align: center;">
+              <p><input type="submit" value="ログイン" class='btn btn-primary'></p><br>
+              <p>または、<a href="{{route('register')}}">新規会員登録</a></p>
+              </div>
+            <div class="row">
+              <div class="col-md-8 offset-md-3">
+              @if($errors->count())
+            <ul class="alert" style="color:red; font-size:12px;">
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            @endif
+              </div>
+              </div>
+          </form>
+		                </div>
+		            </div> <!-- wizard container -->
+		        </div>
+	        </div> <!-- row -->
+	    </div> <!--  big container -->
 
 @endsection
