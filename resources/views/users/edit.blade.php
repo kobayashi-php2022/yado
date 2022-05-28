7a6841969d1fb08d7b31353bf89dfbede10ab688
abe0609 <2022php42-student008@la-study.com>
2022-05-28 23:25:35 +0900

    mypage05/28

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <h1>会員情報変更</h1>
+@include('commons/error_flash')
     <form action="{{route ('users.update', $user->id)}}" method="post">
         @method('patch')
         @csrf
@@ -12,11 +13,10 @@
                     <div class="card-header">会員ID：{{$user->id}}</div>
                     <div class="card-body">
     <tr>
-        <td>お名前:<input type="text" name="name" value="{{old('name', $user->name)}}" cols="30"><br></td>
-        <td>住所:<input type="text" name="address" value="{{old('address', $user->address)}}"cols="30" ><br></td>
-        <td>生年月日:<input type="text" name="birth" value="{{old('birth', $user->birth)}}"cols="30" ><br></td>
-        <td>電話番号:<input type="tel" name="tel" value="{{old('tel', $user->tel)}}"cols="30" ><br></td>
-        <td>メールアドレス:<input type="email" name="email" value="{{old('email', $user->email)}}" cols="30"><br></td>
+        <td>お名前:<input type="text" name="name" value="{{old('name', $user->name)}}" ><br></td>
+        <td>住所:<input type="text" name="address"  value="{{old('address', $user->address)}}"><br></td>
+        <td>電話番号:<input type="tel" name="tel" pattern="\d{1,5}-\d{1,4}-\d{4,5}" title="電話番号は市外局番からハイフン（-）をいれて入力してください" value="{{old('tel', $user->tel)}}"><br></td>
+        <td>メールアドレス:<input type="email" name="email" title="末尾には@〇〇〇〇などのメールアドレスの形式で入力してください" value="{{old('email', $user->email)}}" ><br></td>
     </tr>
 </p>
 </div>
@@ -25,3 +25,4 @@
 </form>
 
 @endsection
+{{-- minlength="1" maxlength="100" --}}
