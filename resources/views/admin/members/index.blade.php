24576d96687514631ed6e57f391303005893d6dc
morisuzuka-2022 <2022php42-student007@la-study.com>
2022-05-29 14:08:23 +0900

    自分帯ウラン地に追加

---
+++ resources/views/admin/members/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+<form action="{{ route('members.index') }}" method="get">
+    @csrf
+    <div class="p-3 mb-2 bg-secondary text-white">会員検索</div>
+    <dl>
+        <dt>ID検索：</dt>
+        <dl><input type="number" name="id" id="id" min="1" max="300" value="{{ request('id') }}"></dl>
+        <dt>名前検索：</dt>
+        <dl><input type="text" name="name" id="name" value="{{ request('name') }}"></dl>
+        <p><input type="submit" value="検索"></p>
+    </form>
+    <div class="p-3 mb-2 bg-secondary text-white">会員一覧</div>
+    @foreach ($users as $member)
+        <div class="info_index">
+                <table style="margin-left: 30px;">
+                    <tr>
+                        <th>id</th>
+                        <th><a href="{{ route('members.show', $member->id) }}">{{ $member->id }}</a></th>
+                    </tr>
+                    <tr>
+                        <td>名前</td>
+                        <td>{{ $member->name }}</td>
+                    </tr>
+                </table>
+        </div>
+    @endforeach
+{{ $users->appends(Request::all())->links('pagination::bootstrap-4') }}
+@endsection
